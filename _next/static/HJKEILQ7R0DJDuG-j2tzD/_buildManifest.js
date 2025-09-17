09e6e50e6bcb97c6272d99916b6698959f04b0b5
Rahul Kasarla <rahul@example.com>
2025-09-17 20:31:07 +0530

    Updates

---
+++ _next/static/HJKEILQ7R0DJDuG-j2tzD/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/caab6787b9336c7b.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
